a6fa68e84a837f4acb0bbae3ab72a0948bd40761
cuilinhao <15021538370@163.com>
2025-12-11 18:23:53 +0800

    添加谷歌监控

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Providers } from "./providers";
 
@@ -29,6 +30,16 @@ export default function RootLayout({
   return (
     <html lang="zh-CN">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {/* Google Analytics 全站统计 */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-870BNT6QG8" strategy="afterInteractive" />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-870BNT6QG8');
+          `}
+        </Script>
         <Providers>{children}</Providers>
       </body>
     </html>
